b6fd0fc4bcdec997d0b44493c3fc43abc3d5adbd
Bonniebon <jan.vijfhuizen@outlook.com>
2021-11-17 15:27:42 +0100

    WIP added most utility methods to vkRenderer.

--- VkRenderer/Include/VkRenderer/VkRenderer.h
+++ VkRenderer/Include/VkRenderer/VkRenderer.h
@@ -24,7 +24,37 @@ namespace vi
 		explicit VkRenderer(const Settings& settings);
 		~VkRenderer();
 
+		void Submit(VkCommandBuffer* buffers, uint32_t buffersCount,
+			VkSemaphore waitSemaphore = VK_NULL_HANDLE, 
+			VkSemaphore signalSemaphore = VK_NULL_HANDLE, 
+			VkFence fence = VK_NULL_HANDLE) const;
 		void DeviceWaitIdle() const;
+
+		[[nodiscard]] VkImage CreateImage(glm::ivec2 resolution, 
+			VkFormat format = VK_FORMAT_R8G8B8A8_SRGB, 
+			VkImageTiling tiling = VK_IMAGE_TILING_OPTIMAL, 
+			VkImageUsageFlags usage = VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT) const;
+		void DestroyImage(VkImage image) const;
+
+		[[nodiscard]] VkImageView CreateImageView(VkImage image, VkFormat format = VK_FORMAT_R8G8B8A8_SRGB,
+			VkImageAspectFlags aspectFlags = VK_IMAGE_ASPECT_COLOR_BIT) const;
+		void DestroyImageView(VkImageView imageView) const;
+
+		[[nodiscard]] VkFramebuffer CreateFrameBuffer(const VkImageView* imageViews, 
+			uint32_t imageViewCount, VkRenderPass renderPass, VkExtent2D extent) const;
+		void DestroyFrameBuffer(VkFramebuffer frameBuffer) const;
+
+		[[nodiscard]] VkCommandBuffer CreateCommandBuffer() const;
+		void BeginCommandBufferRecording(VkCommandBuffer commandBuffer);
+		void EndCommandBufferRecording() const;
+		void DestroyCommandBuffer(VkCommandBuffer commandBuffer) const;
+
+		[[nodiscard]] VkSemaphore CreateSemaphore() const;
+		void DestroySemaphore(VkSemaphore semaphore) const;
+
+		[[nodiscard]] VkFence CreateFence() const;
+		void WaitForFence(VkFence fence) const;
+		void DestroyFence(VkFence fence) const;
 
 	private:
 		class WindowHandler* _windowHandler;
@@ -36,5 +66,7 @@ namespace vi
 		VkDevice _device;
 		Queues _queues;
 		VkCommandPool _commandPool;
+
+		VkCommandBuffer _currentCommandBuffer;
 	};
 }
--- VkRenderer/Source/VkRenderer.cpp
+++ VkRenderer/Source/VkRenderer.cpp
@@ -62,9 +62,221 @@ namespace vi
 		vkDestroyInstance(_instance, nullptr);
 	}
 
+	void VkRenderer::Submit(VkCommandBuffer* buffers, 
+		const uint32_t buffersCount, const VkSemaphore waitSemaphore,
+		const VkSemaphore signalSemaphore, const VkFence fence) const
+	{
+		VkSubmitInfo submitInfo{};
+		submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
+
+		VkPipelineStageFlags waitStage = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
+		submitInfo.waitSemaphoreCount = waitSemaphore ? 1 : 0;
+		submitInfo.pWaitSemaphores = &waitSemaphore;
+		submitInfo.pWaitDstStageMask = &waitStage;
+		submitInfo.commandBufferCount = buffersCount;
+		submitInfo.pCommandBuffers = buffers;
+		submitInfo.signalSemaphoreCount = signalSemaphore ? 1 : 0;
+		submitInfo.pSignalSemaphores = &signalSemaphore;
+
+		vkResetFences(_device, 1, &fence);
+		const auto result = vkQueueSubmit(_queues.graphics, 1, &submitInfo, fence);
+		assert(!result);
+	}
+
 	void VkRenderer::DeviceWaitIdle() const
 	{
 		const auto result = vkDeviceWaitIdle(_device);
 		assert(!result);
+	}
+
+	VkImage VkRenderer::CreateImage(
+		const glm::ivec2 resolution, 
+		const VkFormat format, 
+		const VkImageTiling tiling,
+		const VkImageUsageFlags usage) const
+	{
+		VkImageCreateInfo imageInfo{};
+		imageInfo.sType = VK_STRUCTURE_TYPE_IMAGE_CREATE_INFO;
+		imageInfo.imageType = VK_IMAGE_TYPE_2D;
+		imageInfo.extent.width = static_cast<uint32_t>(resolution.x);
+		imageInfo.extent.height = static_cast<uint32_t>(resolution.y);
+		imageInfo.extent.depth = 1;
+		imageInfo.mipLevels = 1;
+		imageInfo.arrayLayers = 1;
+		imageInfo.format = format;
+		imageInfo.tiling = tiling;
+		imageInfo.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+		imageInfo.usage = usage;
+		imageInfo.samples = VK_SAMPLE_COUNT_1_BIT;
+		imageInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
+
+		VkImage image;
+		const auto result = vkCreateImage(_device, &imageInfo, nullptr, &image);
+		assert(!result);
+		return image;
+	}
+
+	void VkRenderer::DestroyImage(const VkImage image) const
+	{
+		vkDestroyImage(_device, image, nullptr);
+	}
+
+	VkImageView VkRenderer::CreateImageView(const VkImage image, 
+		const VkFormat format, const VkImageAspectFlags aspectFlags) const
+	{
+		VkImageViewCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		createInfo.image = image;
+
+		createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+		createInfo.format = format;
+
+		createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+		createInfo.subresourceRange.aspectMask = aspectFlags;
+		createInfo.subresourceRange.baseMipLevel = 0;
+		createInfo.subresourceRange.levelCount = 1;
+		createInfo.subresourceRange.baseArrayLayer = 0;
+		createInfo.subresourceRange.layerCount = 1;
+
+		VkImageView imageView;
+		const auto result = vkCreateImageView(_device, &createInfo, nullptr, &imageView);
+		assert(!result);
+		return imageView;
+	}
+
+	void VkRenderer::DestroyImageView(const VkImageView imageView) const
+	{
+		vkDestroyImageView(_device, imageView, nullptr);
+	}
+
+	VkFramebuffer VkRenderer::CreateFrameBuffer(const VkImageView* imageViews, const uint32_t imageViewCount,
+		const VkRenderPass renderPass, const VkExtent2D extent) const
+	{
+		VkFramebufferCreateInfo framebufferInfo{};
+		framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+		framebufferInfo.renderPass = renderPass;
+		framebufferInfo.attachmentCount = imageViewCount;
+		framebufferInfo.pAttachments = imageViews;
+		framebufferInfo.width = extent.width;
+		framebufferInfo.height = extent.height;
+		framebufferInfo.layers = 1;
+
+		VkFramebuffer frameBuffer;
+		const auto result = vkCreateFramebuffer(_device, &framebufferInfo, nullptr, &frameBuffer);
+		assert(!result);
+		return frameBuffer;
+	}
+
+	void VkRenderer::DestroyFrameBuffer(const VkFramebuffer frameBuffer) const
+	{
+		vkDestroyFramebuffer(_device, frameBuffer, nullptr);
+	}
+
+	void VkRenderer::BeginRenderPass(const VkFramebuffer frameBuffer, 
+		const VkRenderPass renderPass, const glm::ivec2 offset,
+		const glm::ivec2 extent, VkClearValue* clearColors, const uint32_t clearColorsCount) const
+	{
+		const VkExtent2D extentVk
+		{
+			static_cast<uint32_t>(extent.x),
+			static_cast<uint32_t>(extent.y)
+		};
+
+		VkRenderPassBeginInfo renderPassInfo{};
+		renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+		renderPassInfo.renderPass = renderPass;
+		renderPassInfo.framebuffer = frameBuffer;
+		renderPassInfo.renderArea.offset = { offset.x, offset.y };
+		renderPassInfo.renderArea.extent = extentVk;
+
+		renderPassInfo.clearValueCount = clearColorsCount;
+		renderPassInfo.pClearValues = clearColors;
+
+		vkCmdBeginRenderPass(_currentCommandBuffer, &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
+	}
+
+	void VkRenderer::EndRenderPass() const
+	{
+		vkCmdEndRenderPass(_currentCommandBuffer);
+	}
+
+	VkCommandBuffer VkRenderer::CreateCommandBuffer() const
+	{
+		VkCommandBufferAllocateInfo allocInfo{};
+		allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+		allocInfo.commandPool = _commandPool;
+		allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
+		allocInfo.commandBufferCount = 1;
+
+		VkCommandBuffer commandBuffer;
+		const auto result = vkAllocateCommandBuffers(_device, &allocInfo, &commandBuffer);
+		assert(!result);
+
+		return commandBuffer;
+	}
+
+	void VkRenderer::BeginCommandBufferRecording(const VkCommandBuffer commandBuffer)
+	{
+		VkCommandBufferBeginInfo beginInfo{};
+		beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+		beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
+
+		vkResetCommandBuffer(commandBuffer, VK_COMMAND_BUFFER_RESET_RELEASE_RESOURCES_BIT);
+		vkBeginCommandBuffer(commandBuffer, &beginInfo);
+
+		_currentCommandBuffer = commandBuffer;
+	}
+
+	void VkRenderer::EndCommandBufferRecording() const
+	{
+		const auto result = vkEndCommandBuffer(_currentCommandBuffer);
+		assert(!result);
+	}
+
+	void VkRenderer::DestroyCommandBuffer(const VkCommandBuffer commandBuffer) const
+	{
+		vkFreeCommandBuffers(_device, _commandPool, 1, &commandBuffer);
+	}
+
+	VkSemaphore VkRenderer::CreateSemaphore() const
+	{
+		VkSemaphoreCreateInfo semaphoreInfo{};
+		semaphoreInfo.sType = VK_STRUCTURE_TYPE_SEMAPHORE_CREATE_INFO;
+
+		VkSemaphore semaphore;
+		const auto result = vkCreateSemaphore(_device, &semaphoreInfo, nullptr, &semaphore);
+		assert(!result);
+		return semaphore;
+	}
+
+	void VkRenderer::DestroySemaphore(const VkSemaphore semaphore) const
+	{
+		vkDestroySemaphore(_device, semaphore, nullptr);
+	}
+
+	VkFence VkRenderer::CreateFence() const
+	{
+		VkFenceCreateInfo fenceInfo{};
+		fenceInfo.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO;
+		fenceInfo.flags = VK_FENCE_CREATE_SIGNALED_BIT;
+
+		VkFence fence;
+		const auto result = vkCreateFence(_device, &fenceInfo, nullptr, &fence);
+		assert(!result);
+		return fence;
+	}
+
+	void VkRenderer::WaitForFence(const VkFence fence) const
+	{
+		vkWaitForFences(_device, 1, &fence, VK_TRUE, UINT64_MAX);
+	}
+
+	void VkRenderer::DestroyFence(const VkFence fence) const
+	{
+		vkDestroyFence(_device, fence, nullptr);
 	}
 }
