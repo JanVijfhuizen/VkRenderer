f2d489a278589359036fa06ddb14a3c7831906c8
Bonniebon <jan.vijfhuizen@outlook.com>
2021-12-10 15:27:56 +0100

    Finished free list allocator.

--- VkRenderer/Include/VkRenderer/FreeListAllocator.h
+++ VkRenderer/Include/VkRenderer/FreeListAllocator.h
@@ -5,94 +5,18 @@ namespace vi
 	class FreeListAllocator final
 	{
 	public:
-		explicit FreeListAllocator(size_t size);
+		explicit FreeListAllocator(size_t capacity);
 		~FreeListAllocator();
 
 		[[nodiscard]] void* Allocate(size_t size);
 		void Free(void* ptr);
 
+		[[nodiscard]] size_t GetCapacity() const;
+		[[nodiscard]] void* GetData() const;
+
 	private:
 		size_t* _data;
-		size_t _size;
+		size_t _capacity;
 		size_t* _next;
 	};
-
-	inline FreeListAllocator::FreeListAllocator(size_t size) : _size(size)
-	{
-		size /= sizeof(size_t);
-
-		_data = new size_t[size];
-		_next = _data;
-
-		*reinterpret_cast<void**>(_data) = nullptr;
-		_next[1] = size - 2;
-	}
-
-	inline FreeListAllocator::~FreeListAllocator()
-	{
-		delete[] _data;
-	}
-
-	inline void* FreeListAllocator::Allocate(size_t size)
-	{
-		size = size / 4 + 2;
-
-		size_t* current = _next;
-
-		while(current)
-		{
-			auto& space = current[1];
-			const auto next = reinterpret_cast<size_t*>(*current);
-
-			if(space < size)
-			{
-				current = next;
-				continue;
-			}
-
-			const size_t diff = space - size;
-
-			if (diff > 1)
-			{
-				space = size;
-
-				const auto partitioned = reinterpret_cast<size_t*>(&current[size + 2]);
-				*reinterpret_cast<size_t**>(current) = partitioned;
-				*reinterpret_cast<size_t**>(partitioned) = next;
-
-				partitioned[1] = diff - 2;
-			}
-
-			if (_next == current)
-				_next = reinterpret_cast<size_t*>(*current);
-			return &current[2];
-		}
-
-		return nullptr;
-	}
-
-	inline void FreeListAllocator::Free(void* ptr)
-	{
-		const auto partition = &reinterpret_cast<size_t*>(ptr)[-2];
-
-		size_t* current = _next;
-
-		while (current)
-		{
-			auto& space = current[1];
-			size_t i = partition[1];
-			size_t j = current[1 + space];
-			
-			const auto adjecent = &current[space + 2];
-
-			if(adjecent == partition)
-			{
-				space += partition[1] + 2;
-				return;
-			}
-
-			const auto next = reinterpret_cast<size_t*>(*current);
-			current = next;
-		}
-	}
 }
---
+++ VkRenderer/Source/FreeListAllocator.cpp
@@ -0,0 +1,107 @@
+#include "pch.h"
+#include "FreeListAllocator.h"
+
+namespace vi
+{
+	FreeListAllocator::FreeListAllocator(size_t capacity) : _capacity(capacity)
+	{
+		capacity /= sizeof(size_t);
+
+		_data = new size_t[capacity];
+		_next = _data;
+
+		*reinterpret_cast<void**>(_data) = nullptr;
+		_next[1] = capacity - 2;
+	}
+
+	FreeListAllocator::~FreeListAllocator()
+	{
+		delete[] _data;
+	}
+
+	void* FreeListAllocator::Allocate(size_t size)
+	{
+		size = size / 4 + 2;
+
+		size_t* current = _next;
+
+		while (current)
+		{
+			auto& space = current[1];
+			const auto next = reinterpret_cast<size_t*>(*current);
+
+			if (space < size)
+			{
+				current = next;
+				continue;
+			}
+
+			const size_t diff = space - size;
+
+			if (diff > 1)
+			{
+				space = size;
+
+				const auto partitioned = reinterpret_cast<size_t*>(&current[size + 2]);
+				*reinterpret_cast<size_t**>(current) = partitioned;
+				*reinterpret_cast<size_t**>(partitioned) = next;
+
+				partitioned[1] = diff - 2;
+			}
+
+			if (_next == current)
+				_next = reinterpret_cast<size_t*>(*current);
+			return &current[2];
+		}
+
+		return nullptr;
+	}
+
+	void FreeListAllocator::Free(void* ptr)
+	{
+		const auto partition = &reinterpret_cast<size_t*>(ptr)[-2];
+		const auto adjecent = &partition[partition[1] + 2];
+
+		size_t* previous = nullptr;
+		size_t* current = _next;
+
+		while (current)
+		{
+			auto& space = current[1];
+
+			if (adjecent == current)
+			{
+				*partition = current[2];
+				partition[1] += space + 2;
+
+				if (_next == current)
+					_next = partition;
+				else
+					*reinterpret_cast<size_t**>(previous) = partition;
+				return;
+			}
+
+			const auto currentAdjecent = &current[space + 2];
+
+			if (currentAdjecent == partition)
+			{
+				space += partition[1] + 2;
+				return;
+			}
+
+			const auto next = reinterpret_cast<size_t*>(*current);
+			previous = current;
+			current = next;
+		}
+	}
+
+	size_t FreeListAllocator::GetCapacity() const
+	{
+		return _capacity;
+	}
+
+	void* FreeListAllocator::GetData() const
+	{
+		return _data;
+	}
+}
