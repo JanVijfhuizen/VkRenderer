42034146dba0833f507e7a4bbd69c634a7b98a6d
Bonniebon <jan.vijfhuizen@outlook.com>
2021-12-17 16:50:05 +0100

    Fixed a bug in mapset.

--- VkEngine/Include/MapSet.h
+++ VkEngine/Include/MapSet.h
@@ -28,7 +28,8 @@ MapSet<Value>::MapSet(const size_t size) : UVector<KeyValuePair<uint32_t, Value>
 template <typename Value>
 void MapSet<Value>::Add(const KeyValuePair<uint32_t, Value>& keyPair)
 {
-	for (size_t i = UVector<KeyValuePair<uint32_t, Value>>::GetCount() - 1; i >= 0; --i)
+	const size_t count = UVector<KeyValuePair<uint32_t, Value>>::GetCount() - 1;
+	for (size_t i = 0; i < count; ++i)
 	{
 		auto& value = UVector<KeyValuePair<uint32_t, Value>>::operator [](i);
 		if (value.key == keyPair.key)
@@ -44,7 +45,8 @@ void MapSet<Value>::Add(const KeyValuePair<uint32_t, Value>& keyPair)
 template <typename Value>
 void MapSet<Value>::Erase(const uint32_t sparseId)
 {
-	for (size_t i = UVector<KeyValuePair<uint32_t, Value>>::GetCount() - 1; i >= 0; --i)
+	const size_t count = UVector<KeyValuePair<uint32_t, Value>>::GetCount() - 1;
+	for (size_t i = 0; i < count; ++i)
 	{
 		auto& value = UVector<KeyValuePair<uint32_t, Value>>::operator [](i);
 		if (value.key != sparseId)
