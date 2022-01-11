0dff7dee1f0e3f0039111a80d3b3d128688beb84
Bonniebon <jan.vijfhuizen@outlook.com>
2022-01-11 14:24:50 +0100

    [Fix] Didn't update header.

--- VkEngine/Include/Rendering/GPULinearAllocator.h
+++ VkEngine/Include/Rendering/GPULinearAllocator.h
@@ -13,6 +13,10 @@ class GPULinearAllocator : public vi::VkHandler
 public:
 	explicit GPULinearAllocator(vi::VkCore& core);
 
+	[[nodiscard]] Memory Allocate(VkImage image, VkMemoryPropertyFlags flags) const;
+	[[nodiscard]] Memory Allocate(VkBuffer buffer, VkMemoryPropertyFlags flags) const;
+	[[nodiscard]] Memory Allocate(VkMemoryRequirements memRequirements, VkMemoryPropertyFlags flags) const;
+
 private:
 	struct Block final
 	{
